f18e0fba543fa973214d690a062f938c26b425a9
Ankit sharma <ankit972125@gmail.com>
2024-08-15 20:49:24 +0530

    sqlite

--- olms/settings.py
+++ olms/settings.py
@@ -77,17 +77,26 @@ WSGI_APPLICATION = 'olms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'olmspythondb',
+#         'USER': 'root',
+#         'PASSWORD': '12345678',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+        
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'olmspythondb',
-        'USER': 'root',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 #DATABASES = {
  #   "default": dj_database_url.parse(os.environ.get('DATABASE_URL'))
